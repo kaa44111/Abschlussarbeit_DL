a95398ebc5b5cd7b3e66116854e378bda02c0fe8
Amina Kasa <amina.kasa@gefasoft.com>
2024-06-28 12:32:45 +0200

    changes: transformations only done in pictures

--- datasets/Geometry_dataset.py
+++ datasets/Geometry_dataset.py
@@ -33,6 +33,9 @@ class CustomDataset(Dataset):
         # Define the label mapping
         self.mapping = mapping
 
+        print(f"Found {len(self.image_files)} images")
+        print(f"Found {len(self.mask_files)} masks")
+
     def __len__(self):
         return len(self.image_files)
 
@@ -48,36 +51,32 @@ class CustomDataset(Dataset):
             masks = [Image.open(os.path.join(self.mask_folder, mask_file)).convert('L') for mask_file in mask_paths]
             masks = [tv_tensors.Mask(torch.from_numpy(np.array(mask)).unsqueeze(0).float() / 255.0) for mask in masks]
 
-            # Create a combined mask where each form gets a unique class
-            combined_mask = torch.zeros_like(masks[0], dtype=torch.long)  # Ensure combined_mask is long type
-            for i, mask in enumerate(masks):
-                if self.mapping:
-                    combined_mask[mask > 0] = self.mapping.get(f"form_{i}", i+1)  # Use external mapping if available
-                else:
-                    combined_mask[mask > 0] = i+1  # Use index if no mapping is provided
+            # # Create a combined mask where each form gets a unique class
+            # combined_mask = torch.zeros_like(masks[0], dtype=torch.long)  # Ensure combined_mask is long type
+            # for i, mask in enumerate(masks):
+            #     if self.mapping:
+            #         combined_mask[mask > 0] = self.mapping.get(f"form_{i}", i+1)  # Use external mapping if available
+            #     else:
+            #         combined_mask[mask > 0] = i+1  # Use index if no mapping is provided
 
             if self.transform:
                 image = self.transform(image)
-                combined_mask = self.transform(combined_mask)
-                masks = [self.transform(mask) for mask in masks]
 
             masks_tensor = torch.stack(masks, dim=0)  # Erzeugt einen Tensor der Form [6, 1, H, W]
             masks_tensor = masks_tensor.squeeze(1)  # Ändert die Form zu [6, H, W]
 
-            return image, combined_mask, masks_tensor
+            return image, masks_tensor
         
         except Exception as e:
             print(f"Error loading data at index {idx}: {e}")
-            return None, None, None  # Return dummy values
+            return None,None  # Return dummy values
         
 
 def get_dataloaders():
 
     transformations = v2.Compose([
     v2.ToPureTensor(),
-    BinningTransform(bin_size=2),  # Beispiel für Binning mit bin_size 2
     v2.ToDtype(torch.float32, scale=True),
-    #PatchTransform(patch_size=64),  # Beispiel für das Aufteilen in Patches der Größe 64x64
     ])
 
     custom_dataset = CustomDataset('data', transform=transformations,mapping=MAPPING)
@@ -91,8 +90,8 @@ def get_dataloaders():
     train_dataset, val_dataset = random_split(custom_dataset, [train_size, val_size])
 
     # Erstellen der DataLoader für Training und Validierung
-    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True,collate_fn=custom_collate_fn)
-    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False,collate_fn=custom_collate_fn)
+    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False)
 
     #Creating Dataloaders:
     dataloaders = {
@@ -106,18 +105,23 @@ if __name__ == '__main__':
     dataloader=get_dataloaders()
 
     try:
-        # dataset = CustomDataset('data',mapping=MAPPING)
-        # sample = dataset[0]
-        # img, comb, mas = sample
-        # print(f"{type(img) = }\n{type(comb) = }\n{type(mas) = }\n{type(mas[0]) = }")
+        transformations = v2.Compose([
+            v2.ToPureTensor(),
+            v2.ToDtype(torch.float32, scale=True),
+            v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ])
+
+        dataset = CustomDataset('data', transform=transformations,mapping=MAPPING)
+        sample = dataset[0]
+        img, mas = sample
+        print(mas.shape)        
 
         # Beispiel für den direkten Zugriff auf das erste Batch
         batch = next(iter(dataloader['train']))
 
-        images, combined_masks, masks = batch
+        images,masks = batch
         print(images.shape)
-        print(combined_masks.shape)
-        print(len(masks[0]), masks.shape)
+        print(masks.shape)
 
 
         #   # Optional: visualize the first sample in the batch
