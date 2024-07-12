7c9aa5b6a08c0210f653724ba139b371d0468785
Amina Kasa <amina.kasa@gefasoft.com>
2024-07-12 11:36:58 +0200

    Devide RetinaVessel Data into smaller patches

--- prepare/prepare_patches.py
+++ prepare/prepare_patches.py
@@ -2,17 +2,37 @@ import os
 from PIL import Image
 import numpy as np
 
+
+# def create_patches(image, patch_size):
+#     '''
+#     Teilt ein Bild in Patches auf und füllt Ränder mit Nullen auf, um die gleiche Größe zu gewährleisten.
+#     '''
+#     width, height = image.size
+#     patches = []
+#     for i in range(0, width, patch_size):
+#         for j in range(0, height, patch_size):
+#             box = (i, j, min(i + patch_size, width), min(j + patch_size, height))
+#             patch = image.crop(box)
+            
+#             # Create a new patch with the desired size and paste the cropped patch
+#             padded_patch = Image.new('RGB' if image.mode == 'RGB' else 'L', (patch_size, patch_size))
+#             padded_patch.paste(patch, (0, 0))
+#             patches.append(padded_patch)
+    
+#     return patches
+
 def create_patches(image, patch_size):
     '''
-    Teilt ein Bild in Patches auf.
+    Teilt ein Bild in Patches auf. Randbehandlung : Der Rand wird einfach weggelassen
     '''
     width, height = image.size
     patches = []
     for i in range(0, width, patch_size):
         for j in range(0, height, patch_size):
-            box = (i, j, min(i + patch_size, width), min(j + patch_size, height))
-            patch = image.crop(box)
-            patches.append(patch)
+            box = (i, j, i + patch_size, j + patch_size)
+            if box[2] <= width and box[3] <= height:
+                patch = image.crop(box)
+                patches.append(patch)
     return patches
 
 def save_patches(patches, base_name, output_folder, prefix):
@@ -31,8 +51,13 @@ def process_images_and_masks(image_folder, mask_folder, output_image_folder, out
     Teilt die Bilder und Masken in Patches auf und speichert sie.
     '''
     for image_name in os.listdir(image_folder):
+        if not image_name.endswith('.tif'):
+            continue
+        
+        base_name = os.path.splitext(image_name)[0]
         image_path = os.path.join(image_folder, image_name)
-        mask_path = os.path.join(mask_folder, image_name)
+        mask_name = f"{base_name}_1.bmp"
+        mask_path = os.path.join(mask_folder, mask_name)
         
         if not os.path.exists(mask_path):
             print(f"Maske für Bild {image_name} nicht gefunden. Überspringen.")
@@ -44,16 +69,14 @@ def process_images_and_masks(image_folder, mask_folder, output_image_folder, out
         image_patches = create_patches(image, patch_size)
         mask_patches = create_patches(mask, patch_size)
         
-        base_name = os.path.splitext(image_name)[0]
-        
         save_patches(image_patches, base_name, output_image_folder, 'patch')
         save_patches(mask_patches, base_name, output_mask_folder, 'patch')
 
 # Beispielhafte Verwendung
-image_folder = 'prepare/test_binning/grabs'
-mask_folder = 'prepare/test_binning/masks'
-output_image_folder = 'path_to_your_output_folder/modified/grabs'
-output_mask_folder = 'path_to_your_output_folder/modified/masks'
+image_folder = 'data/RetinaVessel/train/grabs'
+mask_folder = 'data/RetinaVessel/train/masks'
+output_image_folder = 'prepare/test_patches/grabs'
+output_mask_folder = 'prepare/test_patches/masks'
 patch_size = 256  # Größe der Patches
 
 process_images_and_masks(image_folder, mask_folder, output_image_folder, output_mask_folder, patch_size)
