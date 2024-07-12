93398a98c048c98abd43a4ee1ee5e3d5b8e69822
Amina Kasa <amina.kasa@gefasoft.com>
2024-07-12 18:30:15 +0200

    test normalize methods

--- prepare/prepare.py
+++ prepare/prepare.py
@@ -8,15 +8,10 @@ if project_path not in sys.path:
     
 import torch
 from PIL import Image
-import numpy as np
-import torchvision.transforms.functional as F
 from torchvision.transforms import v2
 from torchvision import transforms
-import matplotlib.pyplot as plt
-from torchvision import tv_tensors
-from torch.utils.data import Dataset, DataLoader
 from datasets.WireCheck_dataset import CustomDataset
-from tqdm import tqdm
+from utils.data_utils import show_normalized_images ,show_image_and_mask, compute_mean_std, compute_mean_std_from_dataset
 
 def downsample_image(input_path, output_path, scale_factor):
     '''
@@ -64,140 +59,108 @@ def find_masks_to_image(image_folder,mask_folder,scale_factor):
         mask_name = os.path.join(mask_folder, mask_files[idx])
         downsample_image(mask_name,f"{mask_modified}/{mask_files[idx]}",scale_factor)
 
-
-def compute_mean_std_test(dataset):
-    '''
-    Berechnet mean und std der Images in einem Dataset.
-    '''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=6, shuffle=False, num_workers=0)
-
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    total_pixels = 0
-
-    for images, _ in dataloader:
-        # Reshape to (batch_size * height * width, channels)
-        images = images.permute(0, 2, 3, 1).reshape(-1, 3)
-        
-        # Update total number of pixels
-        total_pixels += images.shape[0]
-
-        # Sum up mean and std for each channel
-        mean += images.mean(dim=0)
-        std += images.std(dim=0)
-
-    # Calculate the mean and std across the entire dataset
-    mean /= len(dataloader)
-    std /= len(dataloader)
-
-    return mean, std
-
-def compute_mean_std(image_folder):
-    '''
-    Berechnet Mittelwerte und Standardabweichungen der Pixelwerte für die Bilder in einem Ordner.
-    '''
-    # Initialize sums and squared sums for each channel
-    channels_sum = torch.zeros(3)
-    channels_squared_sum = torch.zeros(3)
-    total_pixels = 0
-
-    # Transformation
-    transform = transforms.ToTensor()
-
-    # Iterate over all images in the folder
-    for image_name in tqdm(os.listdir(image_folder)):
-        image_path = os.path.join(image_folder, image_name)
-        image = Image.open(image_path).convert('RGB')
-        image = transform(image)
-        
-        # Sum up the values and their squares
-        channels_sum += image.sum(dim=[1, 2])
-        channels_squared_sum += (image ** 2).sum(dim=[1, 2])
-        total_pixels += image.size(1) * image.size(2)  # Anzahl der Pixel pro Bild
-
-    # Calculate mean and std across the entire dataset
-    mean = channels_sum / total_pixels
-    std = torch.sqrt(channels_squared_sum / total_pixels - mean ** 2)
-
-    return mean, std
-
-def show_image_with_rgb(image_path):
-    '''
-    Zeigt die RGB Werte eines Bildes an.
-    '''
-    # Bild laden und in RGB umwandeln
-    image = Image.open(image_path).convert('RGB')
-    
-    # Bild in ein NumPy-Array umwandeln
-    np_image = np.array(image)
-    
-    # Anzeigen des Bildes
-    plt.imshow(np_image)
-    plt.title("Image")
-    plt.axis('off')
-    plt.show()
-    
-    # Anzeigen der RGB-Werte
-    print("RGB values of the 1st Image:")
-    print(np_image.min() , np_image.max())
-    #print(np_image)
-
-def show_image_and_mask(image, mask):
-    '''
-    Zeigt die Bilder und die dazu gehörigen masken an.
-    '''
-    # Rücktransformieren des Bildes (um die Normalisierung rückgängig zu machen)
-    image = image.numpy().transpose((1, 2, 0))
-
-    # Maske umwandeln
-    mask = mask.squeeze().numpy()
-
-    # Anzeigen des Bildes und der Maske
-    plt.figure(figsize=(12, 6))
-    plt.subplot(1, 2, 1)
-    plt.title("Image")
-    plt.imshow(image)
-    plt.axis('off')
-
-    plt.subplot(1, 2, 2)
-    plt.title("Mask")
-    plt.imshow(mask, cmap='gray')
-    plt.axis('off')
-
-    plt.show()
-
 if __name__ == '__main__':
     try: 
-        # Load images
-        scale_factor = 4  # Verkleinerungsfaktor (z.B. auf 1/4 der ursprünglichen Größe)
-        i_path = 'data/WireCheck/grabs'
-        m_path = 'data/WireCheck/masks'
+        root_dir = 'data_modified/RetinaVessel/train'
+        image_folder = os.path.join(root_dir, 'grabs')  # Verzeichnis mit deinen Bildern
 
-        #find_masks_to_image(i_path,m_path,scale_factor)
+        trans= transforms.Compose([
+            transforms.ToTensor(),
+            v2.RandomEqualize(p=1.0)
+        ])
 
-        image_folder = 'prepare/test_patches/grabs'  # Verzeichnis mit deinen Bildern
-        mean, std = compute_mean_std(image_folder)
+        dataset=CustomDataset(root_dir=root_dir,transform=trans)
+        mean, std = compute_mean_std_from_dataset(dataset)
         print(f"Mean: {mean}")
         print(f"Std: {std}")
 
-        # Beispielhafte Verwendung
-        image_path = 'prepare/test_patches/grabs/1_patch1.tiff'
-        #show_image_with_rgb(image_path)
-
-        trans= v2.Compose([
-            v2.ToPureTensor(),
-            v2.ToDtype(torch.float32, scale=True),
-            #v2.Normalize(mean=mean,std=std),
+        trans= transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean,std=std),
         ])
 
-        dataset1=CustomDataset(root_dir='data/circle_data/train',transform=trans)
-        for i in range(4):
-            image, mask = dataset1[i]
-            #print(image.shape)
-            show_image_and_mask(image,mask)
-            print(mask.min(), mask.max())
+        dataset1=CustomDataset(root_dir=root_dir,transform=trans)
+        image,mask = dataset1[0]
+        print(image.min(), image.max())
 
-       
+        # for i in range(76,80):
+        #     unnormalized_image,_ = dataset[i]
+        #     normalized_image, mask = dataset1[i]
+        #     show_normalized_images(unnormalized_image, normalized_image, mask)
+        #     #print(mask.min(), mask.max())
+
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+#________________________________________________________________
+#########Test different Normailzation methods###########
+# if __name__ == '__main__':
+#     try:
+#         root_dir = 'data_modified/RetinaVessel/train'
+#         image_folder = os.path.join(root_dir, 'grabs')  # Verzeichnis mit deinen Bildern
+
+#         trans= transforms.Compose([
+#             transforms.ToTensor()
+#         ])
+
+#         dataset=CustomDataset(root_dir=root_dir,transform=trans)
+
+#         mean1, std1 = compute_mean_std(image_folder)
+#         mean, std = compute_mean_std_from_dataset(dataset)
+
+#         normalization_methods = {
+#             "Random Equalize": v2.RandomEqualize(p=1.0),  # Anwenden der Histogramm-Gleichverteilung
+#             "Normalize from Dataset" : transforms.Normalize(mean=mean, std=std),
+#             "Normalize from folder" : transforms.Normalize(mean=mean1, std=std1)
+#         }
+
+#         for method_name, method in normalization_methods.items():
+#             print(f"Applying {method_name}...")
+#             # trans = v2.Compose([
+#             #     v2.ToPureTensor(),
+#             #     v2.ToDtype(torch.float32, scale=True),
+#             #     method,
+#             # ])
+
+#             trans = transforms.Compose([
+#                 transforms.ToTensor(),
+#                 method,
+#             ])
+
+#             dataset1 = CustomDataset(root_dir=root_dir, transform=trans)
+
+#              # Anzeigen der Bilder und Masken
+#             for i in range(76,80):
+#                 unnormalized_image, _ = dataset[i]
+#                 normalized_image, mask = dataset1[i]
+#                 if unnormalized_image is None or normalized_image is None or mask is None:
+#                     print(f"Error loading data at index {i}")
+#                     continue
+#                 print(f"Using method: {method_name}")
+#                 show_normalized_images(unnormalized_image, normalized_image, mask)
+
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+
+
+
+#_____________________________________________________
+####### Try binning and saving Images####
+
+
+# if __name__ == '__main__':
+#     try: 
+#         # Load images
+#         scale_factor = 4  # Verkleinerungsfaktor (z.B. auf 1/4 der ursprünglichen Größe)
+#         i_path = 'data/WireCheck/grabs'
+#         m_path = 'data/WireCheck/masks'
+
+#         find_masks_to_image(i_path,m_path,scale_factor)
+
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+
 
 #______________________________________
         # #Bild laden und in RGB umwandeln
@@ -227,7 +190,3 @@ if __name__ == '__main__':
         # plt.show()
 #_________________________________________________
         
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
