65d6a0b206b8cc76561d81321087a42d8bb64016
Amina Kasa <amina.kasa@gefasoft.com>
2024-07-16 19:13:16 +0200

    in elaboration

--- prepare/prepare.py
+++ prepare/prepare.py
@@ -13,52 +13,6 @@ from torchvision import transforms
 from datasets.OneFeature import CustomDataset
 from utils.data_utils import show_normalized_images ,show_image_and_mask, compute_mean_std, compute_mean_std_from_dataset
 
-def downsample_image(input_path, output_path, scale_factor):
-    '''
-    Bilder werden kleiner skaliert
-    '''
-    # Bild laden
-    img = Image.open(input_path)
-    
-    # Neue Größe berechnen
-    new_size = (int(img.width / scale_factor), int(img.height / scale_factor))
-    
-    # Bildgröße ändern (runterskalieren)
-    downsampled_img = img.resize(new_size, Image.Resampling.LANCZOS)
-    
-    # Verkleinertes Bild speichern
-    downsampled_img.save(output_path)
-
-def find_masks_to_image(image_folder,mask_folder,scale_factor):
-    '''
-    Findet die passende Masken die gleich wie die Bilder heißen und führt ein Binning aus. Die verkleinerten Bilder & Masken werden in einem neuen Ordner gespeichert.
-    '''
-    # Liste aller Bilddateien
-    all_image_files = sorted(os.listdir(image_folder), key=lambda x: int(''.join(filter(str.isdigit, x))))
-
-        # Filtere die Bilddateien, für die auch Masken existieren
-    image_files = []
-    mask_files = []
-
-    for image_file in all_image_files:
-        base_name = os.path.splitext(image_file)[0]
-        mask_name = f"{base_name}.tif"
-        if os.path.exists(os.path.join(mask_folder, mask_name)):
-            image_files.append(image_file)
-            mask_files.append(mask_name)
-
-    print(f"Found {len(image_files)} images")
-    print(f"Found {len(mask_files)} masks")
-    image_modified = 'prepare/test/image'
-    mask_modified = 'prepare/test/mask'
-    images = []
-    for idx in range(len(image_files)):
-        img_name = os.path.join(image_folder,image_files[idx])
-        images.append(img_name)
-        downsample_image(img_name,f"{image_modified}/{image_files[idx]}",scale_factor)
-        mask_name = os.path.join(mask_folder, mask_files[idx])
-        downsample_image(mask_name,f"{mask_modified}/{mask_files[idx]}",scale_factor)
-
 if __name__ == '__main__':
     try: 
         root_dir = 'data_modified/RetinaVessel/train'
@@ -139,24 +93,6 @@ if __name__ == '__main__':
 #                     continue
 #                 print(f"Using method: {method_name}")
 #                 show_normalized_images(unnormalized_image, normalized_image, mask)
-
-#     except Exception as e:
-#         print(f"An error occurred: {e}")
-
-
-
-#_____________________________________________________
-####### Try binning and saving Images####
-
-
-# if __name__ == '__main__':
-#     try: 
-#         # Load images
-#         scale_factor = 4  # Verkleinerungsfaktor (z.B. auf 1/4 der ursprünglichen Größe)
-#         i_path = 'data/WireCheck/grabs'
-#         m_path = 'data/WireCheck/masks'
-
-#         find_masks_to_image(i_path,m_path,scale_factor)
 
 #     except Exception as e:
 #         print(f"An error occurred: {e}")
