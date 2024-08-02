0aead62dad29c0a9fa556962039ba0c929f18ddb
Amina Kasa <amina.kasa@gefasoft.com>
2024-08-02 11:43:56 +0200

    train images in float16

--- datasets/Augument_dataset.py
+++ datasets/Augument_dataset.py
@@ -15,7 +15,6 @@ from collections import defaultdict
 import matplotlib.pyplot as plt
 
 from train.train import run
-from test_models.test_model import test
 import numpy as np
 
 import torch
@@ -23,13 +22,16 @@ import torch
 import torch.utils
 from PIL import Image
 import numpy as np
-from utils.data_utils import show_image_and_mask
+#from utils.data_utils import show_image_and_mask, compute_mean_std
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import CutMix, MixUp
 
 class CustomDataset(Dataset):
-    def __init__(self, root_dir, dataset_name=None, transform=None, count=None):
+    def __init__(self, root_dir, dataset_name=None, transform=None, mask_transform=None, count=None):
         self.root_dir = root_dir
         self.dataset_name = dataset_name
         self.transform = transform
+        self.mask_transform = mask_transform
         self.count = count
 
         # Pfade zu den Bildern und Masken
@@ -79,52 +81,82 @@ class CustomDataset(Dataset):
             # Laden des Bildes
             img_name = os.path.join(self.image_folder, self.image_files[idx])
             image = Image.open(img_name).convert('RGB')
-            image = v2.functional.pil_to_tensor(image).float()/ 255.0
+            #image = v2.functional.pil_to_tensor(image).to(torch.float16)/ 255.0
 
             # Laden der Maske für dieses Bild
             mask_name = os.path.join(self.mask_folder, self.mask_files[idx])
             mask = Image.open(mask_name).convert('L')
-            mask = torch.from_numpy(np.array(mask)).unsqueeze(0).float() #/ 255.0
+            #mask = torch.from_numpy(np.array(mask)).unsqueeze(0).to(torch.float16) #/ 255.0
 
-            if self.transform:
+            if self.transform is not None:
+                seed = np.random.randint(2147483647)  # Fix the seed for reproducibility
+                torch.manual_seed(seed)
                 image = self.transform(image)
-                mask = self.transform(mask)
+                
+                torch.manual_seed(seed)
+                mask = self.mask_transform(mask)
+                
 
             return image, mask
 
         except Exception as e:
             print(f"Error loading data at index {idx}: {e}")
             return None, None  # Return dummy values
+        
+def show_image_and_mask(image, mask):
+    fig, ax = plt.subplots(1, 2)
+    ax[0].imshow(image.permute(1, 2, 0).cpu().numpy().astype(np.float32))
+    ax[1].imshow(mask.squeeze().cpu().numpy().astype(np.float32), cmap='gray')
+    plt.show()
 
 '''!!To Do move specific Transformations to __main__ in the train.py or test.py'''
-def get_dataloaders(root_dir,dataset_name=None, batch_size=25, transformations=None, split_size = None):
+def get_dataloaders(root_dir,dataset_name=None, batch_size=None, transformations=None, split_size = None):
     '''
     Default Transform: ToPureTensor(), ToDtype(torch.float32, scale=True)
     Default batch_size : 15
     Default split_size : 0.8
     '''
-    if transformations is None:
-        transformations = v2.Compose([
-            v2.ToPureTensor(),
-            v2.ToDtype(torch.float32, scale=True),
-        ])
+    #mean, std=compute_mean_std('data/data_modified/Dichtflächen/patched/grabs')
     
-    trans1 = v2.Compose([
-        v2.RandomHorizontalFlip(p=0.5),
-        v2.RandomEqualize(p=1.0),
-        v2.ToPureTensor(),
-        v2.ToDtype(torch.float32, scale=True),
+    transformations = v2.Compose([
+        #v2.AutoAugment(v2.AutoAugmentPolicy.IMAGENET),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float16, scale=True),
+    ])
+
+    transformations_mask = v2.Compose([
+        #v2.AutoAugment(v2.AutoAugmentPolicy.IMAGENET),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float16, scale=False),
+    ])
+
+    trans_image1 = v2.Compose([
+        v2.RandomHorizontalFlip(p=1),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float16, scale=True),
+    ])
+
+    trans_image1_mask = v2.Compose([
+        v2.RandomHorizontalFlip(p=1),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float16, scale=False),
     ])
 
     trans2 = v2.Compose([
-        v2.RandomPhotometricDistort(p=1),
-        v2.RandomHorizontalFlip(p=1),
-        v2.ToPureTensor(),
-        v2.ToDtype(torch.float32, scale=True),])
+        v2.RandomVerticalFlip(p=1),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float16, scale=True),
+    ])
 
-    dataset_original = CustomDataset(root_dir=root_dir, dataset_name=dataset_name, transform=transformations)
-    dataset_flip = CustomDataset(root_dir=root_dir, dataset_name=dataset_name, transform=trans1)
-    dataset_distort = CustomDataset(root_dir=root_dir, dataset_name=dataset_name, transform=trans2)
+    trans2_mask = v2.Compose([
+        v2.RandomVerticalFlip(p=1),
+        v2.PILToTensor(),
+        v2.ToDtype(torch.float16, scale=False),
+    ])
+
+    dataset_original = CustomDataset(root_dir=root_dir, dataset_name=dataset_name, transform=transformations, mask_transform=transformations_mask)
+    dataset_flip = CustomDataset(root_dir=root_dir, dataset_name=dataset_name, transform=trans_image1, mask_transform=trans_image1_mask)
+    dataset_distort = CustomDataset(root_dir=root_dir, dataset_name=dataset_name, transform=trans2, mask_transform=trans2_mask)
 
     custom_dataset = ConcatDataset([dataset_original,dataset_flip,dataset_distort])
 
@@ -148,9 +180,12 @@ def get_dataloaders(root_dir,dataset_name=None, batch_size=25, transformations=N
     print(f"Anzahl der Bilder im Trainingsdatensatz: {len(train_dataset)}")
     print(f"Anzahl der Bilder im Validierungsdatensatz: {len(val_dataset)}")
 
+    if batch_size is None:
+        batch_size=15
+
     # Erstellen der DataLoader für Training und Validierung
-    train_loader = DataLoader(train_dataset, batch_size=25, shuffle=True, num_workers=0)
-    val_loader = DataLoader(val_dataset, batch_size=25, shuffle=True, num_workers=0)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
     # Creating Dataloaders:
     dataloaders = {
@@ -164,10 +199,10 @@ if __name__ == '__main__':
      try:
         from models.UNetBatchNorm import UNetBatchNorm
     
-        train_dir = 'data_modified/Dichtflächen/patched'
+        train_dir = 'data/data_modified/Dichtflächen/patched'
         dataset_name = 'Dichtflächen'
     
-        dataloader,custom_dataset = get_dataloaders(root_dir=train_dir,dataset_name=dataset_name)
+        dataloader,custom_dataset = get_dataloaders(root_dir=train_dir,dataset_name=dataset_name,batch_size=10)
 
         batch = next(iter(dataloader['train']))
         images,masks = batch
@@ -177,29 +212,19 @@ if __name__ == '__main__':
         print(f"First image min: {images[0].min()}, max: {images[0].max()}")
         print(f"First mask min: {masks[0].min()}, max: {masks[0].max()}") 
 
-        # Überprüfung der Bilder im Dataloader
-        # for i in range(10,15):
-        #     show_image_and_mask(images[i],masks[i])  
+        #####Überprüfung der Bilder im Dataloader
+        # for i in range(20):
+        #     show_image_and_mask(images[i],masks[i]) 
+        #     print(f"First image min: {images[i].min()}, max: {images[i].max()}")
+        #     print(f"First mask min: {masks[i].min()}, max: {masks[i].max()}")  
 
     #     #_____________________________________________________________
-        save_name = 'test_UNetBatchNorm'
+        save_name = 'test_UNetBatchNorm_Dropout'
 
         ####Training für ein Modell Starten
         print("Train Model with Dichtflächen Dataset:")
         run(UNetBatchNorm, dataloader, dataset_name,save_name)
-
-        trans = v2.Compose([
-            #v2.RandomEqualize(p=1.0),
-            v2.ToPureTensor(),
-            v2.ToDtype(torch.float32, scale=True),
-        ])
         
-    #     results_dir = os.path.join('train/results',dataset_name)
-        trained_model = 'train/results/Dichtflächen/test_UNetBatchNorm.pth'
-        
-        
-        test(UNet=UNetBatchNorm,test_dir=train_dir,transformations = trans,test_trained_model=trained_model)
-
     #     # #______________________________________________________________
 
     #     # #####Training für alle Modelle Starten
